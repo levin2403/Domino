383b76d0de6bb0e07418cf3edd794f9b52b37341
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-10-04 00:06:56 -0700

    Creacion de entidades
    Cree las clases necesarias para las entidades

---
+++ Domino_Dominio/src/main/java/Entidades/Jugador.java
@@ -0,0 +1,14 @@
+package Entidades;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author skevi
+ */
+public class Jugador {
+    
+}
