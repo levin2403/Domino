39175e35e8d015df9dfe1dbb172234e2ac6a6d7e
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-10-04 00:34:20 -0700

    Update Configuracion.java
    Entidad para configuracion terminada

--- Domino_Dominio/src/main/java/Entidades/Configuracion.java
+++ Domino_Dominio/src/main/java/Entidades/Configuracion.java
@@ -5,9 +5,44 @@
 package Entidades;
 
 /**
- *
+ * Clase de configuracion para la partida, declara el numero de jugadores 
+ * y las fichas que se van a repartir
+ * 
  * @author skevi
  */
 public class Configuracion {
+    
+    private static byte numJugadores;
+    private static byte FichasPorJugador;
+
+    /**
+     * Constructor que inicializa ambos atributos de la clase. 
+     * 
+     * @param numJugadores numero de jugadores.
+     * @param FichasPorJugador fichas iniciales por cada jugador.
+     */
+    public Configuracion(byte numJugadores, byte FichasPorJugador) {
+        this.numJugadores = numJugadores;
+        this.FichasPorJugador = FichasPorJugador;
+    }
+
+    /**
+     * Metodo para obetener el numero de jugadores para la partida.
+     * 
+     * @return numero de jugadores.
+     */
+    public byte getNumJugadores() {
+        return numJugadores;
+    }
+
+    /**
+     * Metodo para obetener el numero lwe fichas iniciales por jugador 
+     * para la partida.
+     * 
+     * @return Numero de fichas iniciales a repartir.
+     */
+    public byte getFichasPorJugador() {
+        return FichasPorJugador;
+    }
     
 }
