4b9e6d6556fcd7ceba2df8b779cfd0b0a2436205
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-10-04 21:38:00 -0700

    Tablero
    Partes iniciales que podria ir necesitando el tablero, aun falta poder ver o añadir alguna validacion de lo que se ande colocando conjunto a las fichas

--- Domino_Dominio/src/main/java/Entidades/Tablero.java
+++ Domino_Dominio/src/main/java/Entidades/Tablero.java
@@ -1,15 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Entidades;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author skevi
+ * @author Ruzzky
  */
+  
+// Maneja una lista de fichas y proporciona métodos para agregar y validar conexiones entre ellas.
+
 public class Tablero {
-    
-    
-    
+
+    private List<FichaTablero> fichasTablero; // Lista que almacena las fichas en el tablero.
+
+    // Constructor por defecto que inicializa la lista de fichas.
+    public Tablero() {
+        this.fichasTablero = new ArrayList<>();
+    }
+
+    // Devuelve la lista de fichas en el tablero.
+    public List<FichaTablero> getFichasTablero() {
+        return fichasTablero;
+    }
+
+    // Establece una nueva lista de fichas en el tablero.
+    public void setFichasTablero(List<FichaTablero> fichasTablero) {
+        this.fichasTablero = fichasTablero;
+    }
+
+    // Agrega una ficha al final de la lista.
+    public void addFichasTablero(FichaTablero fichaT) {
+        fichasTablero.add(fichaT);
+    }
+
+    // Agrega una ficha al inicio de la lista.
+    public void agregarInicio(FichaTablero fichaT) {
+        fichasTablero.add(0, fichaT);
+    }
+
+    // Devuelve la ficha en el extremo derecho del tablero.
+    public FichaTablero getExtremoDerecho() {
+        if (fichasTablero.isEmpty()) {
+            return null; // Si no hay fichas, devuelve null.
+        }
+        return fichasTablero.get(fichasTablero.size() - 1);
+    }
+
+    // Devuelve la ficha en el extremo izquierdo del tablero.
+    public FichaTablero getExtremoIzquierdo() {
+        if (fichasTablero.isEmpty()) {
+            return null; // Si no hay fichas, devuelve null.
+        }
+        return fichasTablero.get(0);
+    }
+
+    // Valida si una ficha mula puede ser agregada al tablero.
+    public boolean validaZonaInical(FichaTablero ficha) {
+        if (ficha.isMula()) {
+            addFichasTablero(ficha);
+            return true; // Se puede agregar la ficha.
+        }
+        return false; // No se puede agregar.
+    }
+
 }
