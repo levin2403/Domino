7c9770dd5cdc3085108e94a6a080077270de10d4
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-10-08 19:59:04 -0700

    TableroDTO
    Dto

---
+++ Domino_Negocio/src/main/java/DTOs/TableroDTO.java
@@ -0,0 +1,30 @@
+
+
+package DTOs;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author Ruzzky
+ */
+public class TableroDTO implements Serializable{
+    private List<FichaDTO> fichasTablero;
+
+    public TableroDTO() {
+    }
+
+    public TableroDTO(List<FichaDTO> fichasTablero) {
+        this.fichasTablero = fichasTablero;
+    }
+
+    public List<FichaDTO> getFichasTablero() {
+        return fichasTablero;
+    }
+
+    public void setFichasTablero(List<FichaDTO> fichasTablero) {
+        this.fichasTablero = fichasTablero;
+    }
+    
+}
