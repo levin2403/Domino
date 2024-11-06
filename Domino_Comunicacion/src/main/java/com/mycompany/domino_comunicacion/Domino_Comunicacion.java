e0a48abbd5da5907ef979a42ffd40678ebfbfc2b
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-11-05 20:08:40 -0700

    creacion de comunicacion
    esta vacio aun. dejenme esa chamba que creo saber como hacerle ya

---
+++ Domino_Comunicacion/src/main/java/com/mycompany/domino_comunicacion/Domino_Comunicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.domino_comunicacion;
+
+/**
+ *
+ * @author Ruzzky
+ */
+public class Domino_Comunicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
