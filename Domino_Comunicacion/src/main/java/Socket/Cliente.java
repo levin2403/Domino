ac5bd3d934ace16732da554220c137a91cb32d8c
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-11-10 19:21:50 -0700

    comunicacion
    ando viendo que ondas para que quede comunicado como debe de ser

--- Domino_Comunicacion/src/main/java/Socket/Cliente.java
+++ Domino_Comunicacion/src/main/java/Socket/Cliente.java
@@ -2,10 +2,76 @@
 
 package Socket;
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  *
  * @author Ruzzky
  */
 public class Cliente {
-    //tiene que haber un cliente Duh
+    
+ // Instancia única y compartida de Cliente
+    private volatile static Cliente instance;
+ // Objeto de conexión al servidor   
+    private Conexion cone;
+    
+    
+// Constructor privado para prevenir instanciación directa
+    private Cliente() {
+        
+    }
+    
+    /**
+     * Obtiene la instancia única de Cliente.
+     * Si la instancia aún no existe, intenta crear una nueva conexión al servidor 
+     * en el puerto especificado (1234) en localhost.
+     *
+     * @return La instancia única de Cliente o null si ocurre un error.
+     */
+    
+    public static synchronized Cliente getInstance() {
+
+        if (instance == null) {
+            Socket socket;
+            try {
+                System.out.println("S");
+                socket = new Socket("localhost", 1234);
+                instance = new Cliente(socket);
+            } catch (IOException ex) {
+
+                return null;
+            }
+
+        }
+        return instance;
+    }
+    
+  
+    
+     public Cliente(Socket socket) {
+
+      
+    }
+     
+   
+    /**
+     * Devuelve el objeto de conexión actual.
+     *
+     * @return La conexión actual (`Conexion`).
+     */
+    public Conexion getCone() {
+        return cone;
+    }
+
+
+    /**
+     * Establece el objeto de conexión.
+     *
+     * @param cone Nueva conexión de tipo `Conexion`.
+     */
+    public void setCone(Conexion cone) {
+        this.cone = cone;
+    }  
 }
+
--- Domino_Comunicacion/src/main/java/Socket/Conexion.java
+++ Domino_Comunicacion/src/main/java/Socket/Conexion.java
@@ -2,10 +2,19 @@
 
 package Socket;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+import java.util.Map;
+
 /**
  *
  * @author Ruzzky
  */
-public class Conexion {
-//pa saber donde sera la conexion
+public class Conexion extends Thread {
+
+    private Socket socket;
+    private BufferedWriter out;
+    private BufferedReader in;
+   
 }
