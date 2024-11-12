443a4736273511f5293da40342bc31405f45f079
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-11-11 18:56:20 -0700

    aun cliente
    iniciando movimientos pa traer

--- Domino_Server/src/main/java/com/mycompany/domino_server/Cliente.java
+++ Domino_Server/src/main/java/com/mycompany/domino_server/Cliente.java
@@ -7,5 +7,7 @@ package com.mycompany.domino_server;
  * @author Ruzzky
  */
 public class Cliente {
-    //no le muevan
+    
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 1234;
 }
