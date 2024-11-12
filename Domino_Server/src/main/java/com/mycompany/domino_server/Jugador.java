d691c9e0d27967d34db62e2ba05fdebae4ca4ec5
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-11-11 19:15:41 -0700

    ahi ando
    dejen veo algo que me ondie

--- Domino_Server/src/main/java/com/mycompany/domino_server/Jugador.java
+++ Domino_Server/src/main/java/com/mycompany/domino_server/Jugador.java
@@ -2,10 +2,22 @@
 
 package com.mycompany.domino_server;
 
+import DTOs.JugadorDTO;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+
 /**
  *
  * @author Ruzzky
  */
-public class Jugador {
+public class Jugador extends Thread {
 
+    private Socket clientSocket;
+    private BufferedWriter out;
+    private BufferedReader in;
+    private Servidor server;
+    private JugadorDTO jugador;
+    
+    //ocupare esto 
 }
--- Domino_Server/src/main/java/com/mycompany/domino_server/Servidor.java
+++ Domino_Server/src/main/java/com/mycompany/domino_server/Servidor.java
@@ -2,10 +2,19 @@
 
 package com.mycompany.domino_server;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ *La clase Servidor representa un servidor que 
+ * maneja muchas conexiones de clientes
+ * Este servidor escucha las conexiones de los clientes y permite 
+ * la configuración y el intercambio de datos entre los jugadores conectados.
  *
  * @author Ruzzky
  */
 public class Servidor {
+    
+    private List<Jugador> clienteJugadores = new ArrayList<>();
 
 }
