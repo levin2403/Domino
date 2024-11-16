b47cc6be576c4fb71e67d993bd0cd95c0fc42248
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-11-15 23:32:07 -0700

    cambios
    algunos cambios a un evento

--- Domino_Eventos/src/main/java/Eventos/EventoBuscarPartida.java
+++ Domino_Eventos/src/main/java/Eventos/EventoBuscarPartida.java
@@ -16,79 +16,50 @@ import DTOs.ConfiguracionDTO;
  * @author skevi
  */
 public class EventoBuscarPartida {
-
     /**
      * Atributo que define la acción específica que se realiza en el 
      * evento de búsqueda.
      */
-    public Acciones accion;
-
-    /**
-     * Atributo booleano que indica si la búsqueda fue exitosa o no.
-     */
-    public boolean respuesta;
-
-    /**
-     * Atributo que contiene un mensaje descriptivo de la respuesta.
-     */
-    public String respuestaString;
-
-    /**
-     * Constructor vacío de la clase EventoBuscarPartida.
-     */
+    public  String TIPO_EVENTO = "eventoBuscarPartida";  
+    private Acciones accion;
+    private boolean respuesta;
+    private String respuestaString;
+    private ConfiguracionDTO configuracion;
+  
     public EventoBuscarPartida() {
     }
 
-    /**
-     * Obtiene la acción asociada al evento de búsqueda.
-     * @return acción realizada en el evento.
-     */
+    public ConfiguracionDTO getConfiguracion() {
+        return configuracion;
+    }
+
+    public void setConfiguracion(ConfiguracionDTO configuracion) {
+        this.configuracion = configuracion;
+    }
+
     public Acciones getAccion() {
         return accion;
     }
-
-    /**
-     * Define la acción a realizar en el evento de búsqueda.
-     * @param accion Acción a ser asignada.
-     */
+    
     public void setAccion(Acciones accion) {
         this.accion = accion;
     }
 
-    /**
-     * Verifica si la respuesta de la búsqueda fue exitosa.
-     * @return true si la respuesta es positiva, false en caso contrario.
-     */
     public boolean isRespuesta() {
         return respuesta;
     }
 
-    /**
-     * Asigna el valor de la respuesta de la búsqueda.
-     * @param respuesta Valor booleano de la respuesta (true para éxito, false para fallo).
-     */
     public void setRespuesta(boolean respuesta) {
         this.respuesta = respuesta;
     }
 
-    /**
-     * Obtiene el mensaje descriptivo de la respuesta.
-     * @return Cadena de texto que representa la respuesta.
-     */
     public String getRespuestaString() {
         return respuestaString;
     }
 
-    /**
-     * Asigna el mensaje descriptivo de la respuesta.
-     * @param respuestaString Mensaje a ser asignado como respuesta.
-     */
     public void setRespuestaString(String respuestaString) {
         this.respuestaString = respuestaString;
     }
-
-    public void setConfiguracion(ConfiguracionDTO c) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
     
+    //es mas sencilla ya cosa oiga, tampoco se complique la vida 
 }
