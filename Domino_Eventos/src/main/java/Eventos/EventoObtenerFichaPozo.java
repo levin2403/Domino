2bad729cfb1b6adfcde6d83e86d2f0dbeae0eb82
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-12-02 11:34:47 -0700

    eventos
    cosas agregadas donde hacia falta por los eventos de obtener fichapozo y en el de realizar jugada

--- Domino_Eventos/src/main/java/Eventos/EventoObtenerFichaPozo.java
+++ Domino_Eventos/src/main/java/Eventos/EventoObtenerFichaPozo.java
@@ -4,10 +4,102 @@
  */
 package Eventos;
 
+import DTOs.FichaDTO;
+import DTOs.JugadorDTO;
+
 /**
  *
  * @author skevi
+ * @author RUZZKY
+ * 
+ * El kevin me trajo en madrugada chambeando madremia :´v
  */
+
+/**
+ * Clase que representa un evento para obtener una ficha del pozo en el juego de dominó.
+ * 
+ * Este evento se genera cuando un jugador toma una ficha del pozo.
+ * Contiene información sobre el tipo de evento, el jugador involucrado 
+ * y la ficha obtenida.
+ */
+
 public class EventoObtenerFichaPozo {
-    
+
+    /**
+     * Tipo del evento, utilizado para identificar esta acción específica.
+     */
+    public String TIPO_EVENTO = "eventoObtenerFichaPozo";
+
+    /**
+     * Representa al jugador que obtiene la ficha del pozo.
+     */
+    public JugadorDTO jugador;
+
+    /**
+     * Representa la ficha que el jugador obtiene del pozo.
+     */
+    public FichaDTO ficha;
+
+    /**
+     * Constructor vacío de la clase.
+     * 
+     * Se utiliza principalmente para frameworks que requieren instanciación sin argumentos
+     * o para inicializar los valores posteriormente.
+     */
+    public EventoObtenerFichaPozo() {
+    }
+
+    /**
+     * Obtiene el tipo de evento.
+     */
+    public String getTIPO_EVENTO() {
+        return TIPO_EVENTO;
+    }
+
+    /**
+     * Establece el tipo de evento.
+     * 
+     * @param TIPO_EVENTO El tipo de evento a asignar.
+
+     */
+    public void setTIPO_EVENTO(String TIPO_EVENTO) {
+        this.TIPO_EVENTO = TIPO_EVENTO;
+    }
+
+    /**
+     * Obtiene el jugador asociado al evento.
+     * 
+     * @return El jugador que tomó la ficha del pozo.
+     */
+    public JugadorDTO getJugador() {
+        return jugador;
+    }
+
+    /**
+     * Establece el jugador asociado al evento.
+     * 
+     * @param jugador El jugador que participa en este evento.
+     */
+    public void setJugador(JugadorDTO jugador) {
+        this.jugador = jugador;
+    }
+
+    /**
+     * Obtiene la ficha asociada al evento.
+     * 
+     * @return La ficha que el jugador obtuvo del pozo.
+     */
+    public FichaDTO getFicha() {
+        return ficha;
+    }
+
+    /**
+     * Establece la ficha asociada al evento.
+     * 
+     * @param ficha La ficha que el jugador obtuvo del pozo.
+     */
+    public void setFicha(FichaDTO ficha) {
+        this.ficha = ficha;
+    }
 }
+
--- Domino_Eventos/src/main/java/Eventos/EventoRealizarJugada.java
+++ Domino_Eventos/src/main/java/Eventos/EventoRealizarJugada.java
@@ -4,11 +4,166 @@
  */
 package Eventos;
 
+import DTOs.FichaDTO;
+import DTOs.JugadaDTO;
+import DTOs.JugadorDTO;
+
 /**
  *
  * @author skevi
+ * @author Ruzzky
+ * 
+ * el duo desvelado
+ */
+
+
+/**
+ * Clase que representa un evento de realizar una jugada en el juego de dominó.
+ * 
+ * Este evento se genera cuando un jugador realiza una acción (jugada) durante su turno.
  */
 public class EventoRealizarJugada {
-    
-    public  String TIPO_EVENTO = "eventoRealizarJugada"; 
+
+    /**
+     * Tipo del evento, utilizado para identificar esta acción específica.
+     */
+    public String TIPO_EVENTO = "eventoRealizarJugada";
+
+    /**
+     * Representa la jugada que se está realizando.
+     */
+    public JugadaDTO jugada;
+
+    /**
+     * Representa al jugador que realiza la jugada.
+     */
+    public JugadorDTO jugador;
+
+    /**
+     * Representa la ficha que el jugador está utilizando en esta jugada.
+     */
+    public FichaDTO ficha;
+
+    /**
+     * Indica si la jugada se realiza en el lado izquierdo del tablero.
+     * Si es `false`, la jugada se realiza en el lado derecho.
+     */
+    public boolean Izquierda;
+
+    /**
+     * Constructor que inicializa el evento con la jugada proporcionada.
+     * 
+     * @param jugada La jugada que se está realizando.
+     */
+    public EventoRealizarJugada(JugadaDTO jugada) {
+        this.jugada = jugada;
+    }
+
+    /**
+     * Obtiene la jugada asociada al evento.
+     * 
+     * @return La jugada que se está realizando.
+     */
+    public JugadaDTO getJugada() {
+        return jugada;
+    }
+
+    /**
+     * Establece la jugada asociada al evento.
+     * 
+     * @param jugada La jugada a asociar con este evento.
+     */
+    public void setJugada(JugadaDTO jugada) {
+        this.jugada = jugada;
+    }
+
+    /**
+     * Obtiene el jugador que realiza la jugada.
+     * 
+     * @return El jugador que realiza la acción.
+     */
+    public JugadorDTO getJugador() {
+        return jugador;
+    }
+
+    /**
+     * Establece el jugador que realiza la jugada.
+     * 
+     * @param jugador El jugador que realiza la acción.
+     */
+    public void setJugador(JugadorDTO jugador) {
+        this.jugador = jugador;
+    }
+
+    /**
+     * Obtiene el tipo de evento.
+     * 
+     * @return El tipo de evento como cadena de texto.
+     */
+    public String getTIPO_EVENTO() {
+        return TIPO_EVENTO;
+    }
+
+    /**
+     * Establece el tipo de evento.
+     * 
+     * @param TIPO_EVENTO El tipo de evento a asignar.
+     */
+    public void setTIPO_EVENTO(String TIPO_EVENTO) {
+        this.TIPO_EVENTO = TIPO_EVENTO;
+    }
+
+    /**
+     * Obtiene la ficha asociada a la jugada.
+     * 
+     * @return La ficha utilizada en esta jugada.
+     */
+    public FichaDTO getFicha() {
+        return ficha;
+    }
+
+    /**
+     * Establece la ficha asociada a la jugada.
+     * 
+     * @param ficha La ficha a asociar con esta jugada.
+     */
+    public void setFicha(FichaDTO ficha) {
+        this.ficha = ficha;
+    }
+
+    /**
+     * Indica si la jugada se realiza en el lado izquierdo del tablero.
+     * 
+     * @return `true` si la jugada es en el lado izquierdo, `false` en caso contrario.
+     */
+    public boolean isIzquierda() {
+        return Izquierda;
+    }
+
+    /**
+     * Establece si la jugada se realiza en el lado izquierdo del tablero.
+     * 
+     * @param Izquierda `true` si es en el lado izquierdo, `false` en el lado derecho.
+     */
+    public void setIzquierda(boolean Izquierda) {
+        this.Izquierda = Izquierda;
+    }
+
+    /**
+     * Representa el objeto como una cadena de texto, útil para depuración.
+     * 
+     * @return Una representación textual del evento, incluyendo sus atributos principales.
+     */
+    @Override
+    public String toString() {
+        return "EventoRealizarJugada{" +
+                "TIPO_EVENTO=" + TIPO_EVENTO +
+                ", jugada=" + jugada +
+                ", jugador=" + jugador +
+                ", ficha=" + ficha +
+                ", Izquierda=" + Izquierda +
+                '}';
+    }
 }
+
+
