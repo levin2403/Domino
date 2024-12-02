11c199bedb0c2d2543b3f00d392f586e95066f42
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2024-12-02 10:58:09 -0700

    jugadaDTO
    no se por que no se reflejo lo de Jugada

--- Domino_DTO/src/main/java/DTOs/JugadaDTO.java
+++ Domino_DTO/src/main/java/DTOs/JugadaDTO.java
@@ -8,40 +8,108 @@ import java.io.Serializable;
  *
  * @author Ruzzky
  */
-public class JugadaDTO implements Serializable  {
+import java.io.Serializable;
+
+/**
+ * Clase que representa una jugada en el juego.
+ * Contiene información sobre el jugador, la ficha utilizada y 
+ * la dirección en la que se coloca la ficha (izquierda o derecha).
+ * 
+ * Implementa la interfaz Serializable para permitir la persistencia 
+ * y transmisión de objetos de esta clase.
+ */
+public class JugadaDTO implements Serializable {
+    
+    /**
+     * Representa al jugador que realiza la jugada.
+     */
     public JugadorDTO jugador;
+
+    /**
+     * Representa la ficha que se juega.
+     */
     public FichaDTO ficha;
+
+    /**
+     * Indica si la ficha se coloca en el lado izquierdo.
+     * Si es false, la ficha se colocará en el lado derecho.
+     */
     public boolean izquierda;
 
+    /**
+     * Constructor de la clase JugadaDTO.
+     * 
+     * @param jugador El jugador que realiza la jugada.
+     * @param ficha La ficha que se utiliza en la jugada.
+     * @param izquierda Indica si la ficha se coloca en el lado izquierdo.
+     */
     public JugadaDTO(JugadorDTO jugador, FichaDTO ficha, boolean izquierda) {
         this.jugador = jugador;
         this.ficha = ficha;
         this.izquierda = izquierda;
     }
 
+    /**
+     * Obtiene si la jugada se realiza en el lado izquierdo.
+     */
     public boolean isIzquierda() {
         return izquierda;
     }
 
+    /**
+     * Establece si la jugada debe realizarse en el lado izquierdo.
+     */
     public void setIzquierda(boolean izquierda) {
         this.izquierda = izquierda;
     }
 
+    /**
+     * Obtiene el jugador asociado a esta jugada.
+     */
     public JugadorDTO getJugador() {
         return jugador;
     }
 
+    /**
+     * Establece el jugador que realiza la jugada.
+     */
     public void setJugador(JugadorDTO jugador) {
         this.jugador = jugador;
     }
 
+    /**
+     * Obtiene la ficha asociada a esta jugada.
+     */
     public FichaDTO getFicha() {
         return ficha;
     }
 
+    /**
+     * Establece la ficha a utilizar en esta jugada.
+     */
     public void setFicha(FichaDTO ficha) {
         this.ficha = ficha;
     }
-    
 }
 
+/**
+ * ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀ 
+⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀ 
+⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀ 
+⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀ 
+⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀ 
+⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀ 
+⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ 
+⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ 
+⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀ 
+⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀ 
+⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ */
